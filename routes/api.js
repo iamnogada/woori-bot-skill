46221222ecb084d894145f8c8fa5b35182d5ee80
myguddy <seungil.han@gmail.com>
2019-12-12 23:56:22 +0900

    change display

--- routes/api.js
+++ routes/api.js
@@ -41,16 +41,16 @@ router.get('/', function(req, res, next) {
 
 
 var data = {
-    "211" : "예수는 내 힘이요(내삶의 이유라)",
-    "212" : "갈보리 십자가의",
-    "213" : "거룩하신 하나님",
-    "214" : "그곳에서 기도드리네",
-    "215" : "그대를 만난 건 주님의 축복",
-    "216" : "그때 그 무리들이",
-    "217" : "나는 하나님을 예배하는", 
-    "218" : "나의 모습 나의 소유",
-    "219" : "나 주님의 기쁨 되기 원하네",
-    "220" : "나 주의 믿음 갖고"
+    "211" : "211장 예수는 내 힘이요(내삶의 이유라)",
+    "212" : "212장 갈보리 십자가의",
+    "213" : "213장 거룩하신 하나님",
+    "214" : "214장 그곳에서 기도드리네",
+    "215" : "215장 그대를 만난 건 주님의 축복",
+    "216" : "216장 그때 그 무리들이",
+    "217" : "217장 나는 하나님을 예배하는", 
+    "218" : "218장 나의 모습 나의 소유",
+    "219" : "219장 나 주님의 기쁨 되기 원하네",
+    "220" : "220장 나 주의 믿음 갖고"
 }
 
 var defaultReponse ={
@@ -69,16 +69,19 @@ var defaultReponse ={
 var response ={
     "version": "2.0",
     "template": {
-        "outputs": [
-            {
-                "simpleImage": {
-                    "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg",
-                    "altText": "보물상자입니다"
-                }
+      "outputs": [
+        {
+          "basicCard": {
+            "title": "보물상자",
+            "description": "보물상자 안에는 뭐가 있을까",
+            "thumbnail": {
+              "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
             }
-        ]
+          }
+        }
+      ]
     }
-}
+  }
 
 var imageURL =""
 
@@ -91,8 +94,9 @@ router.post('/', function(req, res, next) {
     }else{
         result = {...response}
 
-        result.template.outputs[0].simpleImage.imageUrl=`${req.protocol}://${req.host}/images/sheet/${number}.jpg`;
-        result.template.outputs[0].simpleImage.altText=data[number];
+        result.template.outputs[0].basicCard.title="우리들의 찬양";
+        result.template.outputs[0].basicCard.description=data[number];
+        result.template.outputs[0].basicCard.thumbnail.imageUrl=`${req.protocol}://${req.host}/images/sheet/${number}.jpg`;
     }
     res.json(result)
 });
